5f1b4d864272cf1a7c515ada48e9e8c9722aee10
poofy25 <pandamagxd@gmail.com>
2023-03-01 19:08:54 +0000

    added functionality to workPage Buttons

---
+++ src/components/workPageComponents/wPShowContent.jsx
@@ -0,0 +1,48 @@
+
+import './workPage.css'
+
+
+
+export  function WorkPageLogoContainer() {
+    
+
+
+
+  return (
+<div className="div">Logo</div>
+  )
+}
+
+export  function WorkPagePostersContainer() {
+    
+
+
+
+    return (
+  <div className="div">Postere</div>
+    )
+  }
+
+  
+export  function WorkPagePostariContainer() {
+    
+
+
+
+    return (
+  <div className="div">Postari</div>
+    )
+  }
+
+  
+export  function WorkPageVidsContainer() {
+    
+
+
+
+    return (
+  <div className="div">Vids</div>
+    )
+  }
+  
+
--- src/components/workPageComponents/workPageComponent.jsx
+++ src/components/workPageComponents/workPageComponent.jsx
@@ -1,31 +1,44 @@
 
+import { useState } from 'react';
 import './workPage.css'
+import { WorkPageLogoContainer , WorkPagePostariContainer , WorkPagePostersContainer , WorkPageVidsContainer } from './wPShowContent.jsx';
 
+const WorkPageShowComponents = {
+  Logos:<WorkPageLogoContainer/>,
+  Posters:<WorkPagePostersContainer/>,
+  Postari:<WorkPagePostariContainer/>,
+  Vids:<WorkPageVidsContainer/>
+}
 
 
 function WorkPageContent() {
     
-
+const [currentWorkShow , setWorkShow] = useState(WorkPageShowComponents.Logos)
 const btnOnClick = (e) =>{
     let prevClickedBtn = document.querySelector(".workBtnActive")
     if(e.target.classList.contains('workBtnInactive')){
+      setWorkShow(e.target.attributes.show.value)
         e.target.classList.remove('workBtnInactive');
         e.target.classList.add('workBtnActive');
         prevClickedBtn.classList.remove('workBtnActive');
         prevClickedBtn.classList.add('workBtnInactive')
     }
-}
 
+
+}
 
   return (
 <div className="workPageContent">
-
   <div className="workButtons">
-   <button className='workBtnActive'   onClick={btnOnClick}>Logouri</button>
-   <button  className='workBtnInactive'  onClick={btnOnClick}>Postări</button>
-   <button className='workBtnInactive' onClick={btnOnClick}>Postere</button>
-   <button  className='workBtnInactive'  onClick={btnOnClick}>Clipuri</button>
+   <button className='workBtnActive'  onClick={btnOnClick} show={'Logos'} >Logouri</button>
+   <button  className='workBtnInactive'  onClick={btnOnClick} show={'Postari'}>Postări</button>
+   <button className='workBtnInactive' onClick={btnOnClick} show={'Postere'} >Postere</button>
+   <button  className='workBtnInactive'  onClick={btnOnClick} show={'Vids'} >Clipuri</button>
   </div>
+  
+ <div className="showWork" >
+   {currentWorkShow}
+ </div>
 
 </div>
   )
