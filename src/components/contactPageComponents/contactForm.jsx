f4695cc3f8e750485a08bee99fabb41c0ab15b14
poofy25 <pandamagxd@gmail.com>
2023-03-07 18:27:22 +0000

    added logic to the contactPage form

--- src/components/contactPageComponents/contactForm.jsx
+++ src/components/contactPageComponents/contactForm.jsx
@@ -1,37 +1,75 @@
 import "./contactPage.css"
+import FormError from "./contactFormError.jsx"
+import { useState } from "react"
 
+
+const errorTexts = {
+  succsseful : "Message send succsesfully!",
+  unsuccsseful : "There was a problem and the text has not been sent!",
+  nameError : "Name must not be blank!",
+  emailError : "Invalid mail adress!",
+  messageError : "Message must not be blank!",
+  timeError : "You sent a message in the last 5 minutes, please try again later!"
+}
 
 
 function ContactForm() {
 
   const serviceID = "service_7qunlpp"
-  const publicKey = "I6wOL8h24eLXq0Ime"
   const templateID = "template_tqn06ds"
-  const form = document.querySelector(".contactFormContainer")
+  
+  const [currentMessageError , setMessageError] = useState()
 
+  const sendEmail = (params)=>{
+
+    emailjs.send(serviceID, templateID, params)
+        .then(function(response) {
+           console.log('SUCCESS!', response.status, response.text);
+           setMessageError(errorTexts.succsseful)
+           let sentDate = Date.now()/1000
+           localStorage.setItem("lastFormSent", sentDate);
+
+        }, function(error) {
+           console.log('FAILED...', error);
+           setMessageError(errorTexts.unsuccsseful)
+        });
+    
+      }
 
   const sendForm = ()=>{
-  console.log("gay")
-  var templateParams = {
+
+
+
+
+
+
+  const templateParams = {
     from_name: document.getElementById("labelName").value,
     email_adress:document.getElementById("labelEmail").value,
     message: document.getElementById("labelMessage").value
   };
- 
 
-  
-emailjs.send(serviceID, templateID, templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+if (templateParams.from_name.length <= 1) return setMessageError(errorTexts.nameError)
+if (templateParams.email_adress.includes('@') === false) return setMessageError(errorTexts.emailError)
+if (templateParams.message.length <= 1) return setMessageError(errorTexts.messageError)
+if (localStorage.getItem("lastFormSent") >= Date.now()/1000 - 10) return setMessageError(errorTexts.timeError)
+
+
+  sendEmail(templateParams);
+
+
+document.getElementById("labelName").value = ""
+document.getElementById("labelEmail").value = ""
+document.getElementById("labelMessage").value = ""
+
+
   }
 
 
 
   return (
     <div className="contactFormContainer">
+     <FormError errorMessage={currentMessageError}/>
         <label>Nume</label>
         <input id="labelName" type="text" name="user_name" placeholder="Nume Prenume"/>
         <label >Email</label>
---
+++ src/components/contactPageComponents/contactFormError.jsx
@@ -0,0 +1,48 @@
+import "./contactPage.css"
+
+const succesfullMessage = {
+    background : "rgba(0, 255, 0 , .75)",
+    border:"rgb(0, 50, 0)"
+}
+
+const unsuccesfullMessage = {
+    background : "rgba(255,0, 0 , .75)",
+    border:"rgb(50, 0, 0)"
+}
+
+function FormError(props) {
+
+
+    let colorStyle = unsuccesfullMessage;
+    const animation = {
+        position:"absolute",
+        opacity:"0"
+    }
+if (props.errorMessage === "Message send succsesfully!"){
+
+colorStyle = succesfullMessage;
+
+animation.position = "relative"
+animation.opacity = "1"
+
+} else if (props.errorMessage !== undefined) {
+animation.position = "relative"
+animation.opacity = "1"
+}
+
+
+  return (
+
+      <div className="errorContainer" style={{
+        backgroundColor: colorStyle.background,
+        border: `1px solid ${colorStyle.border}`,
+        position:animation.position,
+        opacity:animation.opacity
+        }}>
+        <p>{props.errorMessage}</p>
+      </div>
+
+  )
+}
+
+export default FormError
